4c9250bb668efd2813e157f32e9c557cbd5f5106
{joonwooHeo} <{hensher5050@gmail.com}>
2024-11-09 11:31:31 +0900

    messageDB연동 미완

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -2,9 +2,16 @@ const conversationService = require('../services/conversationService');
 
 exports.getResponse = async (req, res) => {
     try {
-        const { text, conversationHistory } = req.body;
+        const { text, converseId, userId, topicId } = req.body;
 
-        const gptResponse = await conversationService.GPTResponse(text, conversationHistory);
+        let conversationId = converseId;
+
+        // 만약 converseId가 없는 경우 새 대화 생성
+        if (!conversationId) {
+            conversationId = await conversationService.createNewConversation(userId, topicId);
+        }
+
+        const gptResponse = await conversationService.GPTResponse(text, conversationId);
         console.log('GPT:', gptResponse);
 
         /*
@@ -12,7 +19,7 @@ exports.getResponse = async (req, res) => {
         console.log(audioFilePath);
         */
 
-        res.json({ text, gptResponse });
+        res.json({ text, gptResponse, converseId: conversationId });
     } catch (error) {
         console.error('대화 중 에러:', error);
         res.status(500).json({ message: '대화 중 에러' });
