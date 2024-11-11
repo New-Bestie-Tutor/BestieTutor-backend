607544f17614f2dfe150d3f05c397e5fd729f482
{joonwooHeo} <{hensher5050@gmail.com}>
2024-11-12 00:03:18 +0900

    DB 에러 미해결

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -2,13 +2,16 @@ const conversationService = require('../services/conversationService');
 
 exports.getResponse = async (req, res) => {
     try {
-        const { text, converseId, email, mainTopic, difficulty } = req.body;
-
+        console.log('요청 데이터:', req.body);
+        
+        const { text, converseId, email, mainTopic, subTopic, difficulty, characterName } = req.body;
+        console.log('전달된 데이터:', { text, converseId, email, mainTopic, subTopic, difficulty, characterName });
         let conversationId = converseId;
 
         // 만약 converseId가 없는 경우 새 대화 생성
         if (!conversationId) {
-            conversationId = await conversationService.createNewConversation(email, mainTopic, difficulty);
+            conversationId = await conversationService.createNewConversation(email, mainTopic, subTopic, difficulty, characterName);
+            console.log('생성된 conversationId:', conversationId);        
         }
 
         const gptResponse = await conversationService.GPTResponse(text, conversationId);
--- models/Message.js
+++ models/Message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
     message_id: { type: Number, unique: true },
-    converse_id: { type: Number, required: true, ref: 'Conversation' }, // Conversation 모델과 연결
+    converse_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Conversation' }, // Conversation 모델과 연결
     message: { type: String, required: true },
     message_type: { 
         type: String, 
