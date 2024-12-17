fe7814cb5c9c21966d80e9629c25b0e350a219bd
{joonwooHeo} <{hensher5050@gmail.com}>
2024-12-18 05:10:30 +0900

    [feat] 사용시간(친밀도) DB

--- models/User.js
+++ models/User.js
@@ -9,7 +9,10 @@ const UserSchema = new mongoose.Schema({
     phone: { type: String, required: true },
     gender: { type: String, required: true },
     address: { type: String, required: true },
-    kakaoId: { type: String, unique: true } // 카카오 아이디 필드 추가
+    kakaoId: { type: String, unique: true }, // 카카오 아이디 필드 추가
+    total_time: { type: Number, default: 0 },
+    quit: { type: Boolean, default: false },
+    delete_date: { type: Date, default: null },
 }, {timestamps: true});
 
 const UserModel = mongoose.model('User', UserSchema);
