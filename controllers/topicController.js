7a414b1b52147aac1b90cc8d9248d7bbb5c1b9e2
gaxunsxo <seoge111493@gmail.com>
2024-12-21 10:36:56 +0900

    update: 관리자 기능 분할할

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -23,20 +23,3 @@ exports.getSubTopics = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 };
-
-// 관리자: 주제 추가
-exports.addTopic = async (req, res) => {
-    const { mainTopic, subTopics } = req.body;
-
-    if (!mainTopic || !Array.isArray(subTopics) || subTopics.length === 0) {
-        throw new Error('대주제와 소주제를 입력해야 합니다.');
-    }
-
-    try {
-        const newTopic = await topicService.addTopic(mainTopic, subTopics);
-        res.status(201).json({ message: '대주제가 추가되었습니다.', topic: newTopic });
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ message: error.message });
-    }
-};
