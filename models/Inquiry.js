a0737d80741804873f85f0f1d74067d30d444792
gaxunsxo <seoge111493@gmail.com>
2024-12-29 15:47:48 +0900

    update: inquiry 스키마 변경

--- models/Inquiry.js
+++ models/Inquiry.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const InquirySchema = new mongoose.Schema({
-    inquiryId: { type: Number, unique: true },
-    email: { type: String, required: true },
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+const InquirySchema = new mongoose.Schema(
+  {
+    userId: { type: Number, ref: "User", required: true },
     category: { type: String, required: true },
-}, {timestamps: true});
+    question: { type: String, required: true },
+    askedAt: { type: Date, default: Date.now },
+    answer: { type: String },
+    answeredAt: { type: Date },
+    isAnswered: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-const InquiryModel = mongoose.model('Inquiry', InquirySchema);
+const InquiryModel = mongoose.model("Inquiry", InquirySchema);
 module.exports = InquiryModel;
