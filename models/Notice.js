dd5684ce8db33cdb3e2fc2facbcf51b6f9b6f15a
gaxunsxo <seoge111493@gmail.com>
2025-01-01 16:10:01 +0900

    feat: notice 스키마 수정

--- models/Notice.js
+++ models/Notice.js
@@ -1,15 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const NoticeSchema = new mongoose.Schema({
-    noticeId: { type: String, unique: true },
+const NoticeSchema = new mongoose.Schema(
+  {
     title: { type: String, required: true },
     content: { type: String, required: true },
-}, {timestamps: true});
+  },
+  { timestamps: true }
+);
 
-NoticeSchema.pre('save', function(next) {
-    this.noticeId = this._id; // _id를 noticeId로 설정
-    next();
-});
-
-const Notice = mongoose.model('Notice', NoticeSchema);
+const Notice = mongoose.model("Notice", NoticeSchema);
 module.exports = Notice;
