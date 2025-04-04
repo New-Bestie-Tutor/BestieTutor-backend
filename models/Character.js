678525e3c8856871413f93efdd0322de8a480e0f
gaxunsxo <seoge111493@gmail.com>
2025-04-04 13:58:47 +0900

    update: Character 모델에 assistant_id 필드 추가가

--- models/Character.js
+++ models/Character.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // 캐릭터 모델 name 이름 appearance 외모 personality 성격 tone 말투
 const characterSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    appearance: { type: String, required: true },
-    personality: { type: String, required: true },
-    tone: { type: String, required: true }
+  assistant_id: { type: String, default: null },
+  name: { type: String, required: true },
+  appearance: { type: String, required: true },
+  personality: { type: String, required: true },
+  tone: { type: String, required: true },
 });
 
-const Character = mongoose.model('Character', characterSchema);
+const Character = mongoose.model("Character", characterSchema);
 
 module.exports = Character;
